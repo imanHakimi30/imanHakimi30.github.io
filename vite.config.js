cc866c3c1ccd11b54fd9ca96033a1544f3201144
iman <20ftt1454@pb.edu.bn>
2022-04-16 10:40:46 +0800

    added base in vite.config.js and script in index.html

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: imanHakimi30.github.io,
   plugins: [vue()]
 })
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/imanHakimi.github.io/'
+    ? '/imanHakimi30.github.io/'
     : '/'
 }
