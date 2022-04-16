8040304ec8a3d6d3e069912b3dd4db06efd5525d
iman <20ftt1454@pb.edu.bn>
2022-04-16 15:21:31 +0800

    testing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: imanHakimi30.github.io,
+  base: imanHakimi30.github.io,
   plugins: [vue()]
 })
