655f5598ca2f4e418922835130a1149118f8c671
iman <20ftt1454@pb.edu.bn>
2022-04-16 10:18:04 +0800

    adddes student details in app.vue

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,28 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="container mx-auto">
+    <p class="bg-sky-500/50 py-2">STUDENT DETAILS</p>
+
+    <div class="bg-[#075985] text-white">
+      <div class="p-8">
+        <img src="./assets/imanz.jpeg" class="object-contain h-50 w-80 mx-auto " alt="image">
+      </div>
+
+      <div class="pb-8">
+        <div class="bg-black py-3 w-80 container mx-auto">
+          <strong>Student Name: Muhammad Nur Iman Hakimi bin Mohammad Nur Adenan</strong><br>
+          <span>Student ID: 20FTT1454</span><br>
+          <span>Course: Level 5 Diploma in Web Development</span><br>
+          <span>School: School of Information, Communication & Techonology</span><br>
+          <span>Group Code: DWDTO6</span><br>
+          <span>Intake 10</span><br>
+        </div>
+      </div>
+
+    </div>
+    <p class="bg-sky-500/50 py-2">sem4_wadt_as3</p>
+  </div>
 </template>
 
 <style>
